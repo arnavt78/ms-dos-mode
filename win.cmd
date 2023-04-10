7359240fd124f02b795a3950722fcc978be1f472
Nicolas Lucien (lucien-rowan) <86562548+Tech-FZ@users.noreply.github.com>
2023-04-10 17:32:36 +0200

    Making the script more flexible

--- win.cmd
+++ win.cmd
@@ -4,4 +4,4 @@ title Restart in Windows Mode (Don't Close this Window!)
 
 msg * "Please wait... running commands. Warning: Do not close the Command Prompt window!"
 
-Powershell.exe -executionpolicy remotesigned -File  C:\Users\arnav\Desktop\windows-mode.ps1
+Powershell.exe -executionpolicy remotesigned -File  .\windows-mode.ps1
