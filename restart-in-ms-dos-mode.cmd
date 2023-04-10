f8e675e45c3fb985d2eeb77459cc2e8c6dabd27e
Nicolas Lucien (lucien-rowan) <86562548+Tech-FZ@users.noreply.github.com>
2023-04-10 17:32:09 +0200

    Making the script more flexible

--- restart-in-ms-dos-mode.cmd
+++ restart-in-ms-dos-mode.cmd
@@ -4,4 +4,4 @@ title Restart in MS-DOS Mode (Don't Close this Window!)
 
 msg * "Please wait... running commands. Warning: Do not close the Command Prompt window!"
 
-Powershell.exe -executionpolicy remotesigned -File  C:\Users\arnav\Desktop\ms-dos-mode.ps1
+Powershell.exe -executionpolicy remotesigned -File  .\ms-dos-mode.ps1
